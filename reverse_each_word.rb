43daf0241c744699278f67096bbc77001bcd3669
Nia Bert <nbert14@gmail.com>
2019-05-22 23:21:03 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+#def reverse_each_word(string)
+ # new_array = []
+  #array = string.split
+#array.each do |word|
+ # new_array << word.reverse 
+#end
+#new_array.join(" ")
+#end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |word|
+    word.reverse
+  end
+ end
